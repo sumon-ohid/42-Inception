5b5124e3653173b31c76ff1d5f56aae660d052e3
Md Ohiduzzaman Sumon <sumon@Mds-MacBook-Air.fritz.box>
2024-09-12 21:10:53 +0200

    v2 works need to check http redirect

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -1,42 +1,31 @@
 #!/bin/sh
 
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
-
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	
-	chown -R mysql:mysql /var/lib/mysql
-
-	# init database
+	echo -e "${GREEN}Creating database...${NC}"
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
-	tfile=`mktemp`
-	if [ ! -f "$tfile" ]; then
+	temp_data=`mktemp`
+	if [ ! -f "$temp_data" ]; then
 		return 1
 	fi
+	cat << DELIM > $temp_data
+	USE mysql;
+	FLUSH PRIVILEGES;
 
-	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
-	cat << EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES;
+	DELETE FROM	mysql.user WHERE User='';
+	DROP DATABASE test;
+	DELETE FROM mysql.db WHERE Db='test';
+	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
-DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_ROOT_PASS';
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_ROOT_PASS';
+	CREATE DATABASE $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+	CREATE USER '$WP_DB_USER'@'%' IDENTIFIED by '$WP_DB_PASS';
+	GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
 
-CREATE DATABASE $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$WP_DB_USER'@'%' IDENTIFIED by '$WP_DB_PASS';
-GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
-
-FLUSH PRIVILEGES;
-EOF
-	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
-	# rm -f $tfile
+	FLUSH PRIVILEGES;
+DELIM
+	/usr/bin/mysqld --user=mysql --bootstrap < $temp_data
 fi
 
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
