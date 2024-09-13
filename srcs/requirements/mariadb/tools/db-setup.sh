1e879c629499d238a87d51dbd96675a9b50c40d4
Md Ohiduzzaman Sumon <sumon@Mds-MacBook-Air.fritz.box>
2024-09-13 18:02:19 +0200

    v3 done, start bonus

--- srcs/requirements/mariadb/tools/db-setup.sh
+++ srcs/requirements/mariadb/tools/db-setup.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	echo -e "${GREEN}Creating database...${NC}"
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
+	echo -e "${GREEN}Creating a temp dir to store data... ${NC}"
 	temp_data=`mktemp`
 	if [ ! -f "$temp_data" ]; then
 		return 1
 	fi
+
+	echo -e "${GREEN}Creating users in database ...🙎🏻‍♂️🔐🙎🏻‍♂️🔐. ${NC}"
 	cat << DELIM > $temp_data
 	USE mysql;
 	FLUSH PRIVILEGES;
@@ -28,7 +34,9 @@ DELIM
 	/usr/bin/mysqld --user=mysql --bootstrap < $temp_data
 fi
 
+echo -e "${GREEN}Modifying mariadb-server.cng file$.... ${NC}"
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
+echo -e "${GREEN}Starting mariadb server... 🗄️ 🌐 ✅${NC}"
 exec /usr/bin/mysqld --user=mysql --console
